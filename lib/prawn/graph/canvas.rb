8a98d019c715eb8485fc8abb2d77dc4eb089bc94
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-06-08 17:11:12 +0900

    Validation and wire in the sizing calculator

--- lib/prawn/graph/canvas.rb
+++ lib/prawn/graph/canvas.rb
@@ -5,6 +5,7 @@ module Prawn
     # as the container in which your chart / graph will be sized to fit within. 
     #
     class Canvas
+      attr_reader :sizing, :series
 
       # @param series [Array[Prawn::Graph::Series]]
       # @param prawn [Prawn::Document]
@@ -12,6 +13,14 @@ module Prawn
       # @return [Prawn::Graph::Canvas] a canvas ready to be drawn on the provided +prawn+ document.
       #
       def initialize(series, prawn, options = {}, &block)
+        @series   = series
+
+        verify_series_are_ok!
+
+        @prawn    = prawn
+        @sizing   = Prawn::Graph::Calculations::DocumentSizing.new([prawn.bounds.width, prawn.bounds.height], options).calculate
+
+        yield self if block_given?
       end
 
       # Fires off the actual drawing of the chart on the provided canvas.
@@ -21,6 +30,20 @@ module Prawn
       end
 
       private
+
+      # Verifies that we provide an array-like object of Prawn::Graph::Series instances to
+      # the Canvas, for later rendering.
+      #
+      def verify_series_are_ok!
+        if @series.respond_to?(:each) && @series.respond_to?(:collect)
+          classes = @series.collect{ |c| c.is_a?(Prawn::Graph::Series) }.uniq
+          if classes.size > 1 || classes[0] != true
+            raise RuntimeError.new("All of the items provided must be instances of Prawn::Graph::Series")
+          end
+        else
+          raise RuntimeError.new("Series provided must be an Array (or Array-like) object.")
+        end
+      end
        
     end
   end
--- spec/prawn/graph/canvas_spec.rb
+++ spec/prawn/graph/canvas_spec.rb
@@ -1,4 +1,46 @@
 require 'spec_helper'
 
 describe Prawn::Graph::Canvas do
+  let(:bounds) { double(width: 800, height: 600) }
+  let(:prawn)  { instance_double(Prawn::Document, bounds: bounds, cursor: 600) }
+
+  describe "when creating a new instance" do
+
+    describe "the invalid data object detection" do
+
+      it "rejects a data object that isn't an array" do
+        data = Object.new
+
+        expect {
+          Prawn::Graph::Canvas.new(data, prawn)
+        }.to raise_error(RuntimeError, "Series provided must be an Array (or Array-like) object.")
+      end
+
+      it "rejects a data object that isn't an array of the correct types" do
+        data = [1,2,3]
+
+        expect {
+          Prawn::Graph::Canvas.new(data, prawn)
+        }.to raise_error(RuntimeError, "All of the items provided must be instances of Prawn::Graph::Series")
+      end
+
+
+      it "rejects a data object that is a mix of correct and incorrect types" do
+        data = [1,Prawn::Graph::Series.new,3]
+
+        expect {
+          Prawn::Graph::Canvas.new(data, prawn)
+        }.to raise_error(RuntimeError, "All of the items provided must be instances of Prawn::Graph::Series")
+      end
+    end
+
+    describe "the instaniated values" do
+      it "sets the instance variables as expected" do
+        subject = Prawn::Graph::Canvas.new([Prawn::Graph::Series.new], prawn)
+
+        expect(subject.instance_variable_get :@prawn).to eq(prawn)
+        expect(subject.instance_variable_get :@sizing).to be_a(Prawn::Graph::Calculations::DocumentSizing)
+      end
+    end
+  end
 end
