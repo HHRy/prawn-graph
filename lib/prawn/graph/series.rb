a39a63a5c278c3c25a4b0e8e9aef47d2a5be20be
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-06-08 15:20:50 +0900

    Document and mark deprecation in Series

--- lib/prawn/graph/series.rb
+++ lib/prawn/graph/series.rb
@@ -18,19 +18,29 @@ module Prawn
         @values << value
       end
 
+      # @return [Integer] The smallest value stored in the +values+ of this Series.
+      #
       def min
         @values.min || 0
       end
 
+      # @return [Integer] The largest value stored in the +values+ of this Series.
+      #
       def max
         @values.max || 0
       end
 
+      # @return [Integer] The size of the +values+ stored in this Series.
+      #
       def size
         @values.size
       end
 
+      # @deprecated Provided to allow for tempory backwards compatibilty with legacy graph drawing. Do not use.
+      # @return [Array] Series represented as an array in the format [ title, val1, val2... ]
+      #
       def to_a
+        warn "[DEPRECATION] Series#to_a is deprecated and will be removed in a future version of prawn-graph."
         [title, @values].compact.flatten
       end
     end
