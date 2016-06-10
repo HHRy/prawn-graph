b73c25acc0f3445cbbf63b812a57b72291be739f
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-06-10 18:41:45 +0900

    Series renderer should be a module rather than a class

--- lib/prawn/graph/chart_components/series_renderer.rb
+++ lib/prawn/graph/chart_components/series_renderer.rb
@@ -5,34 +5,28 @@ module Prawn
       # The Prawn::Graph::ChartComponents::SeriesRenderer is used to plot indivdual Prawn::Graph::Series on
       # a Prawn::Graph::ChartComponents::Canvas and its associated Prawn::Document.
       #
-      class SeriesRenderer
+      module SeriesRenderer
+        class << self 
+          # @param series [Prawn::Graph::Series]
+          # @param canvas [Prawn::Graph::ChartComponents::Canvas]
+          #
+          def render(series, canvas)
+            raise ArgumentError.new("series must be a Prawn::Graph::Series") unless series.is_a?(Prawn::Graph::Series)
+            raise ArgumentError.new("canvas must be a Prawn::Graph::ChartComponents::Canvas") unless canvas.is_a?(Prawn::Graph::ChartComponents::Canvas)
 
-        # @param series [Prawn::Graph::Series]
-        # @param canvas [Prawn::Graph::ChartComponents::Canvas]
-        #
-        def initialize(series, canvas)
-          raise ArgumentError.new("series must be a Prawn::Graph::Series") unless series.is_a?(Prawn::Graph::Series)
-          raise ArgumentError.new("canvas must be a Prawn::Graph::ChartComponents::Canvas") unless canvas.is_a?(Prawn::Graph::ChartComponents::Canvas)
+            @series = series
+            @canvas = canvas
+          end
 
-          @series = series
-          @canvas = canvas
+          private
+
+          # Calculates the relative height of a given point based on the maximum value present in
+          # the series.
+          #
+          def point_height_percentage(value)
+            ((BigDecimal(value, 10)/BigDecimal(@series.max, 10)) * BigDecimal(100)).round(2)
+          end
         end
-
-        # Draws the series on the provided canvas.
-        #
-        def draw
-
-        end
-
-        private
-
-        # Calculates the relative height of a given point based on the maximum value present in
-        # the series.
-        #
-        def point_height_percentage(value)
-          ((BigDecimal(value, 10)/BigDecimal(@series.max, 10)) * BigDecimal(100)).round(2)
-        end
-
       end
     end
   end
