6bf7aff0c95cf42631ff411b8813bbeeedfa2a31
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-07-20 11:05:40 +0900

    Method extract

--- lib/prawn/graph/chart_components/bar_chart_renderer.rb
+++ lib/prawn/graph/chart_components/bar_chart_renderer.rb
@@ -12,6 +12,59 @@ module Prawn
 
         private
 
+        def mark_average_line(series_index)
+          if @series[series_index].mark_average?
+            average_y_coordinate = (point_height_percentage(@series[series_index].avg) * @plot_area_height) - 5
+            prawn.line_width = 1
+            prawn.stroke_color = @color[series_index]
+            prawn.dash(2)
+            prawn.stroke_line([0, average_y_coordinate], [ @plot_area_width, average_y_coordinate ])
+            prawn.undash
+          end
+        end
+
+        def mark_maximum_point(series_index, point, max_marked)
+          mm = max_marked.dup
+
+          if @series[series_index].mark_maximum? && max_marked == false && @series[series_index].values[point] == @series[series_index].max
+            prawn.save_graphics_state do
+              prawn.fill_color = @canvas.theme.max
+              prawn.stroke_color = @canvas.theme.max
+              prawn.line_width = 1
+
+              prawn.dash(2)
+              prawn.stroke_line([x_position, 0], [x_position, y_position])
+              prawn.undash
+
+              prawn.fill_ellipse([x_position, y_position ], 2)
+              mm = true
+            end
+          end
+
+          mm
+        end
+
+        def mark_minimum_point(series_index, point, min_marked)
+          mm = min_marked.dup
+
+          if @series[series_index].mark_minimum? && min_marked == false && !@series[series_index].values[point].zero? && @series[series_index].values[point] == @series[series_index].min
+            prawn.save_graphics_state do
+              prawn.fill_color = @canvas.theme.min
+              prawn.stroke_color = @canvas.theme.min
+              prawn.line_width = 1
+
+              prawn.dash(2)
+              prawn.stroke_line([x_position, 0], [x_position, y_position])
+              prawn.undash
+
+              prawn.fill_ellipse([x_position, y_position ], 2)
+              mm = true
+            end
+          end
+
+          mm
+        end
+
         def render_the_chart
           prawn.bounding_box [@graph_area.point[0] + 5, @graph_area.point[1] - 20], width: @plot_area_width, height: @plot_area_height do
          
@@ -37,44 +90,9 @@ module Prawn
 
                   prawn.fill_and_stroke_line([ x_position ,0], [x_position ,y_position]) unless @series[series_index].values[point].zero?
 
-                  if @series[series_index].mark_average?
-                    average_y_coordinate = (point_height_percentage(@series[series_index].avg) * @plot_area_height) - 5
-                    prawn.line_width = 1
-                    prawn.stroke_color = @color[series_index]
-                    prawn.dash(2)
-                    prawn.stroke_line([0, average_y_coordinate], [ @plot_area_width, average_y_coordinate ])
-                    prawn.undash
-                  end
-
-                  if @series[series_index].mark_minimum? && min_marked == false && !@series[series_index].values[point].zero? && @series[series_index].values[point] == @series[series_index].min
-                    prawn.save_graphics_state do
-                      prawn.fill_color = @canvas.theme.min
-                      prawn.stroke_color = @canvas.theme.min
-                      prawn.line_width = 1
-
-                      prawn.dash(2)
-                      prawn.stroke_line([x_position, 0], [x_position, y_position])
-                      prawn.undash
-
-                      prawn.fill_ellipse([x_position, y_position ], 2)
-                      min_marked = true
-                    end
-                  end
-
-                  if @series[series_index].mark_maximum? && max_marked == false && @series[series_index].values[point] == @series[series_index].max
-                    prawn.save_graphics_state do
-                      prawn.fill_color = @canvas.theme.max
-                      prawn.stroke_color = @canvas.theme.max
-                      prawn.line_width = 1
-
-                      prawn.dash(2)
-                      prawn.stroke_line([x_position, 0], [x_position, y_position])
-                      prawn.undash
-
-                      prawn.fill_ellipse([x_position, y_position ], 2)
-                      max_marked = true
-                    end
-                  end
+                  mark_average_line(series_index)
+                  max_marked = mark_maximum_point(series_index, point, max_marked)
+                  min_marked = mark_minimum_point(series_index, point, min_marked)
                 end
 
               end
