455b828b3561e4b6058599408b3f05a4a6b47f2e
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-07-20 12:30:45 +0900

    MEthod extraction

--- lib/prawn/graph/chart_components/line_chart_renderer.rb
+++ lib/prawn/graph/chart_components/line_chart_renderer.rb
@@ -13,6 +13,49 @@ module Prawn
 
         private
 
+        def mark_previous_as_minimum(min_marked, previous_value, previous_x_offset, previous_y)
+          if @series.mark_minimum? && min_marked == false && previous_value != 0 && previous_value == @series.min 
+            draw_marker_point(@canvas.theme.min, previous_x_offset, previous_y)
+            min_marked = true
+          end
+          min_marked
+        end
+
+        def mark_current_as_minimum(min_marked, this_value, this_x_offset, this_y)
+          if @series.mark_minimum? && min_marked == false && this_value != 0 && this_value == @series.min 
+            draw_marker_point(@canvas.theme.min, this_x_offset, this_y)
+            min_marked = true
+          end
+          min_marked
+        end
+
+        def mark_previous_as_maximum(max_marked, previous_value, previous_x_offset, previous_y)
+          if @series.mark_maximum? && max_marked == false && previous_value != 0 && previous_value == @series.max
+            draw_marker_point(@canvas.theme.max, previous_x_offset, previous_y)
+            max_marked = true
+          end
+          max_marked
+        end
+
+        def mark_current_as_maximum(max_marked, this_value, this_x_offset, this_y)
+          if @series.mark_maximum? && max_marked == false && this_value != 0 && this_value == @series.max
+            draw_marker_point(@canvas.theme.max, this_x_offset, this_y)
+            max_marked = true
+          end
+          max_marked
+        end
+
+        def mark_average_line
+          if @series.mark_average?
+            average_y_coordinate = (point_height_percentage(@series.avg) * @plot_area_height) - 5
+            prawn.line_width = 1
+            prawn.stroke_color = @color
+            prawn.dash(2)
+            prawn.stroke_line([0, average_y_coordinate], [ @plot_area_width, average_y_coordinate ])
+            prawn.undash
+          end
+        end
+
         def render_line_chart
           prawn.bounding_box [@graph_area.point[0] + 5, @graph_area.point[1] - 20], width: @plot_area_width, height: @plot_area_height do
             j = 2
@@ -30,7 +73,6 @@ module Prawn
                 prawn.fill_color    = @color
                 prawn.stroke_color  = @color
 
-
                 previous_value = @series.values[i - 1]
                 this_value = v
 
@@ -47,36 +89,15 @@ module Prawn
                 prawn.fill_ellipse([ ( previous_x_offset), previous_y ], 1)  unless previous_value.zero? || this_value.zero?
                 prawn.fill_ellipse([ ( this_x_offset), this_y ], 1)  unless previous_value.zero? || this_value.zero?
 
-                if @series.mark_minimum? && min_marked == false && previous_value != 0 && previous_value == @series.min 
-                  draw_marker_point(@canvas.theme.min, previous_x_offset, previous_y)
-                  min_marked = true
-                end
+                min_marked = mark_previous_as_minimum(min_marked, previous_value, previous_x_offset, previous_y)
+                min_marked = mark_current_as_minimum(min_marked, this_value, this_x_offset, this_y)
+                max_marked = mark_previous_as_maximum(max_marked, previous_value, previous_x_offset, previous_y)
+                max_marked = mark_current_as_maximum(max_marked, this_value, this_x_offset, this_y)
 
-                if @series.mark_maximum? && max_marked == false && previous_value != 0 && previous_value == @series.max
-                  draw_marker_point(@canvas.theme.max, previous_x_offset, previous_y)
-                  max_marked = true
-                end
-           
-                if @series.mark_minimum? && min_marked == false && this_value != 0 && this_value == @series.min 
-                  draw_marker_point(@canvas.theme.min, this_x_offset, this_y)
-                  min_marked = true
-                end
-
-                if @series.mark_maximum? && max_marked == false && this_value != 0 && this_value == @series.max
-                  draw_marker_point(@canvas.theme.max, this_x_offset, this_y)
-                  max_marked = true
-                end
                 j += 1
               end
               
-              if @series.mark_average?
-                average_y_coordinate = (point_height_percentage(@series.avg) * @plot_area_height) - 5
-                prawn.line_width = 1
-                prawn.stroke_color = @color
-                prawn.dash(2)
-                prawn.stroke_line([0, average_y_coordinate], [ @plot_area_width, average_y_coordinate ])
-                prawn.undash
-              end
+              mark_average_line
             end
             render_axes
           end
